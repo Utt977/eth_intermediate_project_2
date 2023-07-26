3b91b2be6a6cafafbf0eb1d8347dfd27548827db
Uttam Kumar <77450295+Utt977@users.noreply.github.com>
2023-07-26 23:10:58 +0530

    added some changes in code as required review

--- contracts/ATM.sol
+++ contracts/ATM.sol
@@ -1,60 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
-contract Assessment {
+contract ATM {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event FundsTransferred(address indexed recipient, uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
+        uint256 _previousBalance = balance;
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
+        assert(balance == _previousBalance + _amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+        assert(balance == (_previousBalance - _withdrawAmount));
+    }
+
+    function transferFunds(address payable recipient, uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balance >= amount, "Insufficient balance in the contract");
+        recipient.transfer(amount);
+        balance -= amount;
+        emit FundsTransferred(recipient, amount);
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return balance;
     }
 }
